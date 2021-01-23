4d341817aa7047475badd1c0d7d4efba789c9d94
JiwonKo <rhwldnjs1226@gmail.com>
2021-01-23 21:27:19 +0900

    Add: survey app 생성 - model.py 작성 - 설문조사view, template 작성 - custom 어드민 template 추가 - 문항 타입에 따른 질문 폼 생성 - 답변 내용 데이터베이스에 저장

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Survey(models.Model):
+    title = models.CharField(max_length = 256)
+    type  = models.ForeignKey('SurveyType', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    question = models.CharField(max_length = 256)
+    survey   = models.ForeignKey(Survey, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.question
+
+class SurveyType(models.Model):
+    name = models.CharField(max_length = 24)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    phone_number = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.phone_number
+
+class SurveyAnswer(models.Model):
+    user     = models.ForeignKey(User, on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
